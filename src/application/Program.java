23d23d2c82ea09436893296174878523a1f9571f
kleberunesp <kleber.c.silva@unesp.br>
2023-08-10 14:22:28 -0300

    update Implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n=== TEST 5: Seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed!");
+		
 	}
 
 }
